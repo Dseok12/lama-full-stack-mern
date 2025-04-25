b6d68601fa56ae74a4d0e82343eefeda874b379e
D.seok <ehskstkrjsa@naver.com>
2025-04-25 15:23:12 +0900

    lama pinterest

--- src/components/leftBar/leftBar.jsx
+++ src/components/leftBar/leftBar.jsx
@@ -1,8 +1,29 @@
-import './LeftBar.css';
+import './leftBar.css';
 
 const LeftBar = () => {
     return (
-        <div className='leftBar'>LeftBar</div>
+        <div className='leftBar'>
+            <div className="menuIcons">
+                <a href="/" className='menuIcon'>
+                    <img src="/general/logo.png" alt="로고" className="logo" />
+                </a>
+                <a href="/" className='menuIcon'>
+                    <img src="/general/home.svg" alt="홈" />
+                </a>
+                <a href="/" className='menuIcon'>
+                    <img src="/general/create.svg" alt="만들기" />
+                </a>
+                <a href="/" className='menuIcon'>
+                    <img src="/general/updates.svg" alt="수정" />
+                </a>
+                <a href="/" className='menuIcon'>
+                    <img src="/general/messages.svg" alt="메시지" />
+                </a>
+            </div>
+            <a href="/" className='menuIcon'>
+                <img src="/general/messages.svg" alt="" />
+            </a>
+        </div>
     )
 }
 
--- src/components/topBar/topBar.jsx
+++ src/components/topBar/topBar.jsx
@@ -1,8 +1,17 @@
-import './TopBar.css';
+import './topBar.css';
+import UserButton from '../userButton/userButton.jsx';
 
 const TopBar = () => {
     return (
-        <div className='topBar'>TopBar</div>
+        <div className='topBar'>
+            {/* SEARCH */}
+            <div className="search">
+                <img src="/general/search.svg" alt="검색 돋보기" />
+                <input type="text" placeholder='검색' />
+            </div>
+            {/* USER */}
+            <UserButton />
+        </div>
     )
 }
 
